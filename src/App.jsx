d2646a9391182423bcd090170fe099b164654c38
Shubham Chhangani <shubhamchhangani1998@gmail.com>
2023-09-08 13:41:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="bg-gray-700 font-bold text-center text-white text-3xl m-4 p-4">
+      Hello lets build Netflix
+    </div>
+  )
+}
+
+export default App
