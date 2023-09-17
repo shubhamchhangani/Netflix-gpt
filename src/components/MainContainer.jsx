c2282ec38c63c1976066fd014b1d2043e5ffbddf
Shubham Chhangani <shubhamchhangani1998@gmail.com>
2023-09-17 07:59:31 +0530

    css applied

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -8,13 +8,13 @@ export default function MainContainer() {
         return;
     }
     const mainMovie = movies[0];
-    console.log(mainMovie);
+    
 
-    const { original_title, overview } = mainMovie;
+    const { original_title, overview, id } = mainMovie;
     return (
       <div className="pt-[30%] bg-black-100 md:pt-0">
         <VideoTitle title={original_title} overview={overview} />
-        <VideoBackground />
+        <VideoBackground movieId={id} />
       </div>
     );
 }
--- src/components/ViderBackground.jsx
+++ src/components/ViderBackground.jsx
@@ -1,7 +1,23 @@
-export default function VideoBackground() {
-    return (
-        <div>
-            VideoBackground
-        </div>
-    )
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../Hooks/useMovieTrailer";
+
+export default function VideoBackground({ movieId }) {
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+
+  useMovieTrailer(movieId);
+
+  return (
+    <div className=" w-screen">
+      <iframe
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          "pBk4NYhWNMM?si=-5DeWsLWuLANJzz7" +
+          "?&autoplay=1&mute=1&loop=1"
+        }
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
 }
